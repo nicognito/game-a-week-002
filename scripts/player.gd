44ca4e024618ab15495def9d708c23e9662b4c03
Nicolas Mai <nicognito@users.noreply.github.com>
2023-09-04 20:11:56 -0700

    Implement player moves

---
+++ scripts/player.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+
+@export var speed = 250
+
+func _physics_process(delta):
+	var motion = Vector2(Input.get_axis("move_left", "move_right") * speed * delta, 0)
+	move_and_collide(motion)
